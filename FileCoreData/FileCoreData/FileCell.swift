090fd89594b412d1c4ce52a577e12a1cb9006677
Abdullah AlRashoudi <Abdullah.Mansoor.ruh.s@tuwaiq.edu.sa>
2021-11-25 19:38:59 +0300

    Adding File With CoreData

---
+++ FileCoreData/FileCoreData/FileCell.swift
@@ -0,0 +1,24 @@
+//
+//  FileCell.swift
+//  FileCoreData
+//
+//  Created by Abdullah AlRashoudi on 11/24/21.
+//
+
+import UIKit
+
+class FileCell: UITableViewCell {
+
+    @IBOutlet weak var labelCell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
