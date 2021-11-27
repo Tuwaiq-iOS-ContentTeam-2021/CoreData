87e051c1a6a63385e87829979235bf82be34788e
Taraf Bin suhaim <tarafbinsuhaim@Tarafs-MacBook-Pro.local>
2021-11-27 16:48:58 +0300

    CoreData

---
+++ ProductCoreData/ProductCoreData/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  ProductCoreData
+//
+//  Created by Taraf Bin suhaim on 19/04/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
