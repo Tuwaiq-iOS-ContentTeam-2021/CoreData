d6358fc48dabc0670f7078c448ee5fd091b93425
abdullah <shaeen364@gmail.com>
2021-11-27 22:37:48 +0300

    corePro

---
+++ CorePro/CorePro/TVCell.swift
@@ -0,0 +1,24 @@
+//
+//  TVCell.swift
+//  CorePro
+//
+//  Created by Qahtani's MacBook Pro on 11/25/21.
+//
+
+import UIKit
+
+class TVCell: UITableViewCell {
+
+    @IBOutlet weak var labelcell: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
